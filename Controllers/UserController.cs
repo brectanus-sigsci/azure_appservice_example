1d90615cd5015a23a046ed6685300d6f2ae1f6fc
Imran <imran@signalsciences.com>
2017-10-06 15:25:46 -0600

    Initial commit of sample ASP MVC app

---
+++ Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample_MVCApp.Models;
+
+namespace Sample_MVCApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        private static Users _users = new Users();
+        public ActionResult Index()
+        {
+            return View(_users._userList);
+        }
+        public ActionResult UserAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserAdd(UserModels userModel)
+        {
+            _users.CreateUser(userModel);
+            return View();
+        }
+       
+        
+    }
+}
