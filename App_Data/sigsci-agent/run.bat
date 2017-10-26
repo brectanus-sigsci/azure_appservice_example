542d2b5c2356b4a17371a623f8e308dc88c5e4e9
Imran <imran@signalsciences.com>
2017-10-26 17:18:30 -0600

    deploy agent

--- App_Data/sigsci-agent/run.bat
+++ App_Data/sigsci-agent/run.bat
@@ -1,2 +1,1 @@
-echo Attempting to start agent
-REM D:\home\site\wwwroot\App_Data\sigsci-agent\sigsci-agent.exe -config D:\home\site\wwwoot\App_Data\sigsci-agent\agent.conf
+D:\home\site\wwwroot\App_Data\sigsci-agent\sigsci-agent.exe -config D:\home\site\wwwroot\App_Data\sigsci-agent\agent.conf
