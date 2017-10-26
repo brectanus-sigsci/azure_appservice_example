f2dc33634642cb40b291ce49e272df9594f860be
Imran <imran@signalsciences.com>
2017-10-26 11:41:33 -0600

    Trying again

--- App_Data/sigsci-agent/run.bat
+++ App_Data/sigsci-agent/run.bat
@@ -1,1 +1,1 @@
-App_Data\sigsci-agent\sigsci-agent.exe -config App_Data\sigsci-agent\agent.conf
+D:\home\site\wwwroot\App_Data\sigsci-agent\sigsci-agent.exe -config D:\home\site\wwwoot\App_Data\sigsci-agent\agent.conf
