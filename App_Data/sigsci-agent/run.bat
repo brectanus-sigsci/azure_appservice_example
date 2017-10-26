e34e2ce98e5d5d0ababf41604a32cc69a68b6316
Imran <imran@signalsciences.com>
2017-10-26 14:41:21 -0600

    trying to get batch file to run

--- App_Data/sigsci-agent/run.bat
+++ App_Data/sigsci-agent/run.bat
@@ -1,2 +1,2 @@
-echo "Attempting to start agent"
-D:\home\site\wwwroot\App_Data\sigsci-agent\sigsci-agent.exe -config D:\home\site\wwwoot\App_Data\sigsci-agent\agent.conf
+echo Attempting to start agent
+REM D:\home\site\wwwroot\App_Data\sigsci-agent\sigsci-agent.exe -config D:\home\site\wwwoot\App_Data\sigsci-agent\agent.conf
